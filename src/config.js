66fcaed7cd4a13015b0cfb82316905ed2cb5ebf7
mbenic <mbenic@hotmail.com>
2026-04-18 18:45:06 +0800

    remove trailing slash that was causing problem

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 /* export const API_URL = "http://localhost:8000"; */
-export const API_URL = "https://crowd-funding-backend-django.vercel.app/";
+export const API_URL = "https://crowd-funding-backend-django.vercel.app";

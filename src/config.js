910249c40fcc226e46c30ce1ad9e5843862bdc9e
mbenic <mbenic@hotmail.com>
2026-04-21 22:35:46 +0800

    removed trailing slash

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = "http://localhost:8000";
 //export const API_URL = "https://crowd-funding-backend-django.vercel.app";
-export const API_URL = "https://crowdfunding-backend-heroku-5efc7c8b3097.herokuapp.com/";
+export const API_URL = "https://crowdfunding-backend-heroku-5efc7c8b3097.herokuapp.com";

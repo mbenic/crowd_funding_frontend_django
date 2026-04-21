43f89e59f6a4c98a2e376032b5d32724b91c77c0
mbenic <mbenic@hotmail.com>
2026-04-21 23:09:53 +0800

    change to url in edit project file

--- src/pages/EditProject.jsx
+++ src/pages/EditProject.jsx
@@ -50,7 +50,7 @@ export default function EditProject() {
 
     try {
       const response = await fetch(
-        `http://127.0.0.1:8000/api/projects/${id}/`,
+        `${API_URL}/api/projects/${id}/`,
         {
           method: "PUT",
           headers: {
